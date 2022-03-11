9c1dc2143f7f0ccc2212a41f125fbaab48b8ecd9
Your Name <you@example.com>
2022-03-11 06:42:06 +0100

    nknk\

--- requests1.py
+++ requests1.py
@@ -1,12 +1,20 @@
-from cgi import print_arguments
-
-
-try:
+"""try:
  num1 = 10
  num2 = 0
  print(num1/num2)
 except ZeroDivisionError:
         print("bro")
+        raise
 finally:
-        print_arguments
-        "dlmln,vwdscv,ldsm"
+        print('this code will run no matter what')"""
+"""        
+print(1)
+assert 2 + 2 == 4
+print(2)
+assert 1 + 1 == 3
+print(3)
+"""
+# write mode
+nono_file = open("nono.txt", "r")
+nono_file.read()
+nono_file.close()
